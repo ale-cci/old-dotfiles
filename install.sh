388985a82358fe3e777e6ff8490d234a20c3c0dc
ale-cci <alecrd98@gmail.com>
2019-03-20 21:27:06 +0100

    Added pacman configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-# Download Dependencies
+#!/bin/sh
+
 set -e
+# Use all cores for compilation.
+sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
+grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color/Color/" /etc/pacman.conf
+grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+# Download Dependencies
 ./sync-dirs.sh
 ./sync-packages.sh
 ./sync-symlinks.sh
@@ -22,3 +29,5 @@ sudo systemctl enable mpd
 # Setting Wallpaper
 cp wall.png ~/.config
 git config --global merge.tool vimdiff
+
+
