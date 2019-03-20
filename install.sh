1f140e36dccccba82541705b88e027357367c40c
ale-cci <alecrd98@gmail.com>
2019-03-20 01:09:19 +0100

    Fixes

--- install.sh
+++ install.sh
@@ -7,10 +7,6 @@
 [[ $SHELL != "$(which zsh)" ]] && chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Installing ZSH theme
-export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # Vim Configuration
 # Installing Vundle
--- sync-packages.sh
+++ sync-packages.sh
@@ -29,6 +29,7 @@ sudo pacman -S \
 	pulsemixer\
 	python\
 	rsync\
+	npm\
 	subversion\
 	tmux\
 	unrar\
@@ -95,8 +96,14 @@ function suckless_git_install {
 suckless_git_install "https://github.com/LukeSmithxyz/st.git" "$HOME/dl/st-source"
 suckless_git_install "https://github.com/LukeSmithxyz/dmenu.git" "$HOME/dl/dmenu-source"
 
+export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Installing ZSH theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
 # # Powerline fonts instsllation
 # git clone https://github.com/powerline/fonts.git --depth=1
 # cd fonts
